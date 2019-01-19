48ba71a709fe162180185b02012f4c6c055860a7
Antao Almada <antao.almada@gmail.com>
2019-01-19 12:36:10 +0000

    Add a Conan rewriter plugin project.

---
+++ NetFabric.Hyperlinq.Rewriter.Benchmarks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace NetFabric.Hyperlinq.Rewriter.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var switcher = new BenchmarkSwitcher(new[] {
+                    typeof(QueryBenchmarks),
+                });
+            switcher.Run(args);        
+        }
+    }
+}
---
+++ NetFabric.Hyperlinq.Rewriter/HyperlinqRewriter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+
+namespace NetFabric.Hyperlinq.Rewriter
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class HyperlinqRewriter : CompilationRewriter
+    {
+        public override Compilation Rewrite(CompilationRewriterContext context)
+        {
+            var compilation = context.Compilation;
+
+            // Transform compilation
+
+            return compilation;
+        }
+    }
+}
