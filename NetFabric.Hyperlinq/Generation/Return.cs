ee07728c0ec514bade2463865943d85a9166dd08
Antao Almada <antao.almada@gmail.com>
2019-07-13 11:16:46 +0100

    Do not validate index

--- NetFabric.Hyperlinq/Generation/Return.cs
+++ NetFabric.Hyperlinq/Generation/Return.cs
@@ -32,7 +32,7 @@ namespace NetFabric.Hyperlinq
             {
                 get
                 {
-                    if(index != 0) ThrowHelper.ThrowIndexOutOfRangeException();
+                    if (index != 0) ThrowHelper.ThrowIndexOutOfRangeException();
                     
                     return value;
                 }
