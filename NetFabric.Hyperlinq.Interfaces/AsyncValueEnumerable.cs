19969869433449937b5a2f59fc7f81c0315ea4cd
Antao Almada <antao.almada@gmail.com>
2019-08-10 00:14:55 +0100

    Prototype IAsyncValueEnumerable

---
+++ NetFabric.Hyperlinq.Interfaces/AsyncValueEnumerable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace NetFabric.Hyperlinq
+{
+    public interface IAsyncValueEnumerable<out T, TEnumerator>
+        : IAsyncEnumerable<T>
+        where TEnumerator : struct, IAsyncEnumerator<T>
+    {
+        new TEnumerator GetAsyncEnumerator(CancellationToken cancellationToken = default);
+    }
+}
---
+++ NetFabric.Hyperlinq/Aggregation/Count/Count.AsyncValueEnumerable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetFabric.Hyperlinq
+{
+    public static partial class ValueEnumerable
+    {
+        public static async ValueTask<int> CountAsync<TEnumerable, TEnumerator, TSource>(this TEnumerable source, CancellationToken cancellationToken = default)
+            where TEnumerable : IAsyncValueEnumerable<TSource, TEnumerator>
+            where TEnumerator : struct, IAsyncEnumerator<TSource>
+        {
+            var count = 0;
+            await foreach (var item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+            {
+                checked { count++; }
+            }
+            return count;
+        }
+    }
+}
