c3eb3d7a2750aa4871b4b08255f6ff0b7da4406e
Antao Almada <antao.almada@farfetch.com>
2019-12-13 09:13:55 +0000

    Cleanup tool references

--- NetFabric.Hyperlinq.Interfaces/AsyncValueEnumerable.cs
+++ NetFabric.Hyperlinq.Interfaces/AsyncValueEnumerable.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace NetFabric.Hyperlinq
 {
@@ -9,12 +8,5 @@ namespace NetFabric.Hyperlinq
         where TEnumerator : struct, IAsyncEnumerator<T>
     {
         new TEnumerator GetAsyncEnumerator(CancellationToken cancellationToken = default);
-    }
-
-    public interface IAsyncValueReadOnlyCollection<out T, TEnumerator>
-        : IAsyncValueEnumerable<T, TEnumerator>
-        where TEnumerator : struct, IAsyncEnumerator<T>
-    {
-        ValueTask<int> GetCountAsync(CancellationToken cancellationToken = default);
     }
 }
