e438076e5c683fc18dc6bb196292f87804f88f5c
Antao Almada <antao.almada@gmail.com>
2019-12-15 10:57:16 +0000

    Add ForEachAsync

---
+++ NetFabric.Hyperlinq.UnitTests/Other/ForEach/ForEachAsync.AsyncValueEnumerable.Tests.cs
@@ -0,0 +1,52 @@
+using NetFabric.Assertive;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetFabric.Hyperlinq.UnitTests
+{
+    public class ForEachAsyncAsyncValueEnumerableTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData.Empty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.Single), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.Multiple), MemberType = typeof(TestData))]
+        public void ForEachAsync_With_ValidData_Should_Succeed(int[] source)
+        {
+            // Arrange
+            var wrapped = Wrap.AsAsyncValueEnumerable(source);
+            var expected = 0;
+            MoreLinq.Extensions.ForEachExtension.ForEach(Wrap.AsEnumerable(source), item => expected += item);
+
+            // Act
+            var result = 0;
+            AsyncValueEnumerable.ForEachAsync<Wrap.AsyncValueEnumerable<int>, Wrap.AsyncEnumerator<int>, int>(wrapped, (item, _) =>  { result += item; return default; });
+
+            // Assert
+            result.Must()
+                .BeEqualTo(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.Empty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.Single), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.Multiple), MemberType = typeof(TestData))]
+        public void ForEachAsyncIndex_With_ValidData_Should_Succeed(int[] source)
+        {
+            // Arrange
+            var wrapped = Wrap.AsAsyncValueEnumerable(source);
+            var expected = 0;
+            MoreLinq.Extensions.ForEachExtension.ForEach(Wrap.AsEnumerable(source), (item, index) => expected += item + index);
+
+            // Act
+            var result = 0;
+            AsyncValueEnumerable.ForEachAsync<Wrap.AsyncValueEnumerable<int>, Wrap.AsyncEnumerator<int>, int>(
+                wrapped, 
+                (item, index, _) => { result += item + index; return default; });
+
+            // Assert
+            result.Must()
+                .BeEqualTo(expected);
+        }
+    }
+}
