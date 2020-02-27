4d3a1762e56ffefbe453f4247c77897cef112626
Antao Almada <antao.almada@gmail.com>
2020-02-27 13:36:58 +0000

    Improve coverage

---
+++ NetFabric.Hyperlinq.UnitTests/Conversion/AsEnumerable/AsEnumerable.ValueEnumerable.Tests.cs
@@ -0,0 +1,41 @@
+using NetFabric.Assertive;
+using Xunit;
+
+namespace NetFabric.Hyperlinq.UnitTests
+{
+    public partial class ValueEnumerableTests
+    {
+        [Fact]
+        public void AsEnumerable_With_ValueType_Should_ReturnCopy()
+        {
+            // Arrange
+            var source = new int[0];
+            var wrapped = Wrap.AsValueEnumerable(source);
+
+            // Act
+            var result = ValueEnumerable
+                .AsEnumerable<Wrap.ValueEnumerable<int>, Wrap.Enumerator<int>, int>(wrapped);
+
+            // Assert
+            _ = result.Must()
+                .BeEqualTo(wrapped);
+        }
+        
+        [Fact]
+        public void AsEnumerable_With_ReferenceType_Should_ReturnSame()
+        {
+            // Arrange
+            var source = new int[0];
+            var wrapped = Wrap
+                .AsValueEnumerable(source) as IValueEnumerable<int, Wrap.Enumerator<int>>;
+
+            // Act
+            var result = ValueEnumerable
+                .AsEnumerable<IValueEnumerable<int, Wrap.Enumerator<int>>, Wrap.Enumerator<int>, int>(wrapped);
+
+            // Assert
+            _ = result.Must()
+                .BeSameAs(wrapped);
+        }
+    }
+}
