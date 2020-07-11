a34567b3cf477b3638cedf68ca02128fdb16cd1f
Antao Almada <antao.almada@farfetch.com>
2020-07-11 23:31:31 +0100

    Fix plot exporter

--- LinqBenchmarks/BenchmarkBase.cs
+++ LinqBenchmarks/BenchmarkBase.cs
@@ -3,12 +3,12 @@ using BenchmarkDotNet.Jobs;
 
 namespace LinqBenchmarks
 {
-    //[SimpleJob(RuntimeMoniker.Net48, baseline: true)]
-    //[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [SimpleJob(RuntimeMoniker.Net48, baseline: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
     [SimpleJob(RuntimeMoniker.NetCoreApp50)]
     [MemoryDiagnoser]
     [MarkdownExporterAttribute.GitHub]
-    [RPlotExporter] // requires installation of R (https://benchmarkdotnet.org/articles/configs/exporters.html#plots)
+    [RPlotExporter, CsvMeasurementsExporter] // requires installation of R (https://benchmarkdotnet.org/articles/configs/exporters.html#plots)
     public class BenchmarkBase
 
     {
