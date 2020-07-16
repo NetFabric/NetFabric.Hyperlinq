db177ca14ec734aa0bce0f2de0fef0943e8d62c7
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-07-16 15:49:49 +0200

    Distinct: do "something" during distinct

---
+++ LinqBenchmarks/IntEqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LinqBenchmarks
+{
+    struct IntEqualityComparer : IEqualityComparer<int>
+    {
+        public bool Equals(int x, int y)
+        {
+            return x == y;
+        }
+
+        public int GetHashCode(int obj)
+        {
+            return obj;
+        }
+
+    }
+}
