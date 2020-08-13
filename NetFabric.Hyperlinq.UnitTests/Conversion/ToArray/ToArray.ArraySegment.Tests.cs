857af64d8fe06b4f4bd380e488da5d3f7b2ade89
Antao Almada <antao.almada@farfetch.com>
2020-08-13 14:48:24 +0100

    Add tests for ToArray.ArraySegment

--- NetFabric.Hyperlinq.UnitTests/Conversion/ToArray/ToArray.ArraySegment.Tests.cs
+++ NetFabric.Hyperlinq.UnitTests/Conversion/ToArray/ToArray.ArraySegment.Tests.cs
@@ -68,5 +68,264 @@ namespace NetFabric.Hyperlinq.UnitTests.Conversion.ToArray
             _ = result.Memory
                 .SequenceEqual(expected);
         }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakePredicateEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateMultiple), MemberType = typeof(TestData))]
+        public void ToArray_Predicate_Must_Succeed(int[] source, int skip, int take, Predicate<int> predicate)
+        {
+            // Arrange
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Where(wrapped, predicate.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Where(wrapped, predicate)
+                .ToArray();
+
+            // Assert
+            _ = result.Must()
+                .BeNotSameAs(source)
+                .BeArrayOf<int>()
+                .BeEqualTo(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakePredicateEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateMultiple), MemberType = typeof(TestData))]
+        public void ToArray_Predicate_MemoryPool_Must_Succeed(int[] source, int skip, int take, Predicate<int> predicate)
+        {
+            // Arrange
+            var pool = MemoryPool<int>.Shared;
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Where(wrapped, predicate.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Where(wrapped, predicate)
+                .ToArray(pool);
+
+            // Assert
+            _ = result.Memory
+                .SequenceEqual(expected);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakePredicateAtEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateAtSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateAtMultiple), MemberType = typeof(TestData))]
+        public void ToArray_PredicateAt_Must_Succeed(int[] source, int skip, int take, PredicateAt<int> predicate)
+        {
+            // Arrange
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Where(wrapped, predicate.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Where(wrapped, predicate)
+                .ToArray();
+
+            // Assert
+            _ = result.Must()
+                .BeNotSameAs(source)
+                .BeArrayOf<int>()
+                .BeEqualTo(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakePredicateAtEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateAtSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateAtMultiple), MemberType = typeof(TestData))]
+        public void ToArray_PredicateAt_MemoryPool_Must_Succeed(int[] source, int skip, int take, PredicateAt<int> predicate)
+        {
+            // Arrange
+            var pool = MemoryPool<int>.Shared;
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Where(wrapped, predicate.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Where(wrapped, predicate)
+                .ToArray(pool);
+
+            // Assert
+            _ = result.Memory
+                .SequenceEqual(expected);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakeSelectorEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakeSelectorSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakeSelectorMultiple), MemberType = typeof(TestData))]
+        public void ToArray_Selector_Must_Succeed(int[] source, int skip, int take, NullableSelector<int, string> selector)
+        {
+            // Arrange
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Select(wrapped, selector.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Select(wrapped, selector)
+                .ToArray();
+
+            // Assert
+            _ = result.Must()
+                .BeNotSameAs(source)
+                .BeArrayOf<string>()
+                .BeEqualTo(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakeSelectorEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakeSelectorSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakeSelectorMultiple), MemberType = typeof(TestData))]
+        public void ToArray_Selector_MemoryPool_Must_Succeed(int[] source, int skip, int take, NullableSelector<int, string> selector)
+        {
+            // Arrange
+            var pool = MemoryPool<string>.Shared;
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Select(wrapped, selector.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Select(wrapped, selector)
+                .ToArray(pool);
+
+            // Assert
+            _ = result.Memory
+                .SequenceEqual(expected);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakeSelectorAtEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakeSelectorAtSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakeSelectorAtMultiple), MemberType = typeof(TestData))]
+        public void ToArray_SelectorAt_Must_Succeed(int[] source, int skip, int take, NullableSelectorAt<int, string> selector)
+        {
+            // Arrange
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Select(wrapped, selector.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Select(wrapped, selector)
+                .ToArray();
+
+            // Assert
+            _ = result.Must()
+                .BeNotSameAs(source)
+                .BeArrayOf<string>()
+                .BeEqualTo(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakeSelectorAtEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakeSelectorAtSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakeSelectorAtMultiple), MemberType = typeof(TestData))]
+        public void ToArray_SelectorAt_MemoryPool_Must_Succeed(int[] source, int skip, int take, NullableSelectorAt<int, string> selector)
+        {
+            // Arrange
+            var pool = MemoryPool<string>.Shared;
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Select(wrapped, selector.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Select(wrapped, selector)
+                .ToArray(pool);
+
+            // Assert
+            _ = result.Memory
+                .SequenceEqual(expected);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakePredicateSelectorEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateSelectorSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateSelectorMultiple), MemberType = typeof(TestData))]
+        public void ToArray_Predicate_Selector_Must_Succeed(int[] source, int skip, int take, Predicate<int> predicate, NullableSelector<int, string> selector)
+        {
+            // Arrange
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Where(wrapped, predicate.AsFunc())
+                .Select(selector.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Where(wrapped, predicate)
+                .Select(selector)
+                .ToArray();
+
+            // Assert
+            _ = result.Must()
+                .BeNotSameAs(source)
+                .BeArrayOf<string>()
+                .BeEqualTo(expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.SkipTakePredicateSelectorEmpty), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateSelectorSingle), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.SkipTakePredicateSelectorMultiple), MemberType = typeof(TestData))]
+        public void ToArray_Predicate_Selector_MemoryPool_Must_Succeed(int[] source, int skip, int take, Predicate<int> predicate, NullableSelector<int, string> selector)
+        {
+            // Arrange
+            var pool = MemoryPool<string>.Shared;
+            var (offset, count) = Utils.SkipTake(source.Length, skip, take);
+            var wrapped = new ArraySegment<int>(source, offset, count);
+            var expected = Enumerable
+                .Where(wrapped, predicate.AsFunc())
+                .Select(selector.AsFunc())
+                .ToArray();
+
+            // Act
+            var result = ArrayExtensions
+                .Where(wrapped, predicate)
+                .Select(selector)
+                .ToArray(pool);
+
+            // Assert
+            _ = result.Memory
+                .SequenceEqual(expected);
+        }
     }
 }
