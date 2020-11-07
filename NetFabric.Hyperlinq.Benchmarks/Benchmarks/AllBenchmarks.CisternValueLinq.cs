fc8d70fb4a89a9511f9098c9bc945355a5698776
Paul Westcott <p.westcott@gmail.com>
2020-11-07 11:23:36 +1100

    Added ValueLambda versions for All

--- NetFabric.Hyperlinq.Benchmarks/Benchmarks/AllBenchmarks.CisternValueLinq.cs
+++ NetFabric.Hyperlinq.Benchmarks/Benchmarks/AllBenchmarks.CisternValueLinq.cs
@@ -70,6 +70,73 @@ namespace NetFabric.Hyperlinq.Benchmarks
             listReference
             .OfEnumerable() // *shouldn't* need this!!! only because we are in the NetFabric.Hyperlinq namespace; so c# chooses incorrect overload
             .All(_ => true);
+
+        // ----
+
+        struct AlwaysTrue: IFunc<int, bool> { public bool Invoke(int _) => true; }
+
+        [BenchmarkCategory("Array")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_Array() =>
+            array
+            .OfArray() // *shouldn't* need this!!! only because we are in the NetFabric.Hyperlinq namespace; so c# chooses incorrect overload
+            .All(new AlwaysTrue());
+
+        [BenchmarkCategory("Array")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_Span() =>
+            Enumerable
+            .FromSpan<int[], int>(array, array => array.AsSpan())
+            .All(new AlwaysTrue());
+
+        [BenchmarkCategory("Array")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_Memory() =>
+            memory
+            .OfMemory() // *shouldn't* need this!!! only because we are in the NetFabric.Hyperlinq namespace; so c# chooses incorrect overload
+            .All(new AlwaysTrue());
+
+        [BenchmarkCategory("Enumerable_Value")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_Enumerable_Value() =>
+            enumerableValue
+            .OfEnumeratorConstraint<int, TestEnumerable.Enumerable, TestEnumerable.Enumerable.Enumerator>(x => x.GetEnumerator())
+            .All(new AlwaysTrue());
+
+        [BenchmarkCategory("Collection_Value")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_Collection_Value() =>
+            collectionValue
+            .OfEnumeratorConstraint<int, TestCollection.Enumerable, TestCollection.Enumerable.Enumerator>(x => x.GetEnumerator(), collectionValue.Count)
+            .All(new AlwaysTrue());
+
+        [BenchmarkCategory("List_Value")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_List_Value() =>
+            listValue
+            .OfReadOnlyListConstraint<int, TestList.Enumerable>()
+            .All(new AlwaysTrue());
+
+        [BenchmarkCategory("Enumerable_Reference")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_Enumerable_Reference() =>
+            enumerableReference
+            .OfEnumerable() // *shouldn't* need this!!! only because we are in the NetFabric.Hyperlinq namespace; so c# chooses incorrect overload
+            .All(new AlwaysTrue());
+
+        [BenchmarkCategory("Collection_Reference")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_Collection_Reference() =>
+            collectionReference
+            .OfEnumerable() // *shouldn't* need this!!! only because we are in the NetFabric.Hyperlinq namespace; so c# chooses incorrect overload
+            .All(new AlwaysTrue());
+
+        [BenchmarkCategory("List_Reference")]
+        [Benchmark]
+        public bool CisternValueLinq_ValueLambda_List_Reference() =>
+            listReference
+            .OfEnumerable() // *shouldn't* need this!!! only because we are in the NetFabric.Hyperlinq namespace; so c# chooses incorrect overload
+            .All(new AlwaysTrue());
     }
 }
 
