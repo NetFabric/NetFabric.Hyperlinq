4ddac7680abc75055d2703ddcc626b16b1c091ee
Antão Almada <antao.almada@gmail.com>
2021-03-04 19:57:43 +0000

    Optimize ImmutableArray<> by getting inner array (#341)

--- NetFabric.Hyperlinq/Conversion/AsValueEnumerable/Bindings/System/Collections/Immutable/ImmutableArray.cs
+++ NetFabric.Hyperlinq/Conversion/AsValueEnumerable/Bindings/System/Collections/Immutable/ImmutableArray.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections.Immutable;
+using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 
 namespace NetFabric.Hyperlinq
 {
     public static class ImmutableArrayExtensions
     {
-        // ImmutableArray<TSource> is a value-type that implements IReadOnlyList<TSource>
+        // Get the inner array and return its IValueEnumerable<> wrapper.
+        // Do not use the available AsSpan() or AsMemory() because the enumerables for ReadOnlySpan<> do not
+        // implement IEnumerable<>, restricting its use, and the enumerables for ReadOnlyMemory<> are less efficient 
+        // because they call .Span in each iteration.
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ArrayExtensions.ArraySegmentValueEnumerable<TSource> AsValueEnumerable<TSource>(this ImmutableArray<TSource> source)
+            => source.GetArray().AsValueEnumerable();
         
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ReadOnlyListExtensions.ValueEnumerable<ImmutableArray<TSource>, TSource> AsValueEnumerable<TSource>(this ImmutableArray<TSource> source)
-            => source.AsValueEnumerable<ImmutableArray<TSource>, TSource>();
+        static TSource[] GetArray<TSource>(this ImmutableArray<TSource> source)
+            => Unsafe.As<ImmutableArray<TSource>, TSource[]?>(ref source) ?? Array.Empty<TSource>();
     }
 }
